01b472d49dfde907bc8049a5f935ea1fdea0a304
Axel ATTIA <axel.attia@me.com>
2023-09-13 18:49:43 +0200

    adding readme

--- container/test.py
+++ container/test.py
@@ -7,12 +7,11 @@ from quality import extract_unique_tasks, display_outliers
 from quality import get_bids_data, generate_report_with_plots, generate_figure, generate_figures_motion
 import uuid
 import os
-import pandas as pd
 from pathlib import Path
 import argparse
 
-data_path = "/mnt/extra/data/share/bids_fmriprep_10subj/"
-output_dir="/home/axel/Test2/bids_fmriprep_10subj/report"
+#data_path = "/mnt/extra/data/share/bids_fmriprep_10subj/"
+#output_dir="/home/axel/Test2/bids_fmriprep_10subj/report"
 
 #data_path = "/home/axel/Test2/bids_fmriprep_1subj/"
 #output_dir=os.path.join(data_path, "report")
@@ -22,6 +21,21 @@ output_dir="/home/axel/Test2/bids_fmriprep_10subj/report"
 # args = parser.parse_args()
 
 #data_path = args.data_path
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Process data_path for the script.')
+    parser.add_argument('data_path', type=str, help='Path to the data')
+    parser.add_argument('--fd_threshold', type=float, default=0.5, help='Seuil de FD')
+    parser.add_argument('--dvars_threshold', type=float, default=1.5, help='Seuil de DVARS')
+
+    args = parser.parse_args()
+
+    data_path = args.data_path
+    fd_threshold = args.fd_threshold
+    dvars_threshold = args.dvars_threshold
+    output_dir=os.path.join(data_path, "report")
 
 
 reportlets_dir = Path(output_dir) / "reportlets" / "figures"
@@ -36,9 +50,6 @@ all_tasks = []
 
 display_outliers(all_tables, repetition_times, reportlets_dir, 3, 5)
 
-#global_data, motion_outliers_list, repetition_time, signal, subject_data = read_and_preprocess_data(tasks, all_tables, repetition_times, "global_signal")
-#generate_figure_all(all_tables, repetition_times, 'global_signal', reportlets_dir)
-#generate_figure(all_tables, repetition_times, 'global_signal', reportlets_dir)
 generate_figure(all_tables, repetition_times, 'global_signal', reportlets_dir)
 generate_figure(all_tables, repetition_times, 'csf', reportlets_dir)
 generate_figure(all_tables, repetition_times, 'white_matter', reportlets_dir)
@@ -48,29 +59,7 @@ generate_figure(all_tables, repetition_times, 'framewise_displacement', reportle
 generate_figure(all_tables, repetition_times, 'std_dvars', reportlets_dir)
 generate_figure(all_tables, repetition_times, 'rmsd', reportlets_dir)
 generate_figure(all_tables, repetition_times, 'dvars', reportlets_dir)
-#generate_figure_all(all_tables, repetition_times, 'global_signal', reportlets_dir)
-# global_data, motion_outliers_combined, repetition_time = read_and_preprocess_data(tasks, all_tables, repetition_times, "csf")
-# all_tasks.extend(generate_figure(all_tables, repetition_times, 'csf', reportlets_dir, motion_outliers_combined))
-# global_data, motion_outliers_combined, repetition_time = read_and_preprocess_data(tasks, all_tables, repetition_times, "white_matter")
-# all_tasks.extend(generate_figure(all_tables, repetition_times, 'white_matter', reportlets_dir, motion_outliers_combined))
-# global_data, motion_outliers_combined, repetition_time = read_and_preprocess_data(tasks, all_tables, repetition_times, "a_comp_cor_00")
-#generate_figures_motion(all_tables, repetition_times, ['rot_x', 'rot_y', 'rot_z'], reportlets_dir)
-#all_tasks.extend(generate_figure2(all_tables, repetition_times, ['trans_x', 'trans_y', 'trans_z'], reportlets_dir)) 
-# global_data, motion_outliers_combined, repetition_time = read_and_preprocess_data(tasks, all_tables, repetition_times, "framewise_displacement")
-# all_tasks.extend(generate_figure(all_tables, repetition_times, 'framewise_displacement', reportlets_dir, motion_outliers_combined))
-# global_data, motion_outliers_combined, repetition_time = read_and_preprocess_data(tasks, all_tables, repetition_times, "std_dvars")
-# all_tasks.extend(generate_figure(all_tables, repetition_times, 'std_dvars', reportlets_dir, motion_outliers_combined))
-# global_data, motion_outliers_combined, repetition_time = read_and_preprocess_data(tasks, all_tables, repetition_times, "rmsd")
-# all_tasks.extend(generate_figure(all_tables, repetition_times, 'rmsd', reportlets_dir, motion_outliers_combined))
-# global_data, motion_outliers_combined, repetition_time = read_and_preprocess_data(tasks, all_tables, repetition_times, "dvars")
-# all_tasks.extend(generate_figure(all_tables, repetition_times, 'dvars', reportlets_dir, motion_outliers_combined))
 
-
-#display_motion_outliers(all_tables)
-
-#output_dir = os.path.join(data_path, "report")
-#reportlets_dir = Path(output_dir) / "report" / "reportlets" / sub_id_value  / "figures"
-#reportlets_dir.mkdir(parents=True, exist_ok=True)
 
 
 bootstrap_file="./bootstrap-fmriprep.yml"
@@ -82,12 +71,6 @@ metadata={
         "Provenance Information": prov  
     }
 }
-
-# layout = BIDSLayout(in_folder)
-# entities = {}
-# files = layout.get()
-# for file in files:
-#     entities.update(file.get_entities())
 
 
 report_filename = generate_report_with_plots(
